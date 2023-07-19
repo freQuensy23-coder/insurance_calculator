7ab0aad2aae65925843b5d85c763bdcc79375b1e
freQuensy23-coder <mametevalex@gmail.com>
2023-07-19 23:19:39 +0300

    [IMP] Day of delivery can be selected now

--- PriceCalculator.py
+++ PriceCalculator.py
@@ -1,5 +1,8 @@
+import datetime
 from _decimal import Decimal
 from datetime import date
+from typing import Optional
+
 from database.models import Rate
 from loguru import logger
 from Exceptions import RateDoNotEstablished
@@ -8,22 +11,22 @@ from Exceptions import RateDoNotEstablished
 @staticmethod
 class PriceCalculator:
     """Отвечает за расчет цен в данном приложении"""
-    async def calculate_insurance_cost(self, base_cost: str, cargo_type: str) -> Decimal:
+    async def calculate_insurance_cost(self, base_cost: str, cargo_type: str, delivery_date: Optional[datetime.date] = None) -> Decimal:
         """Calculate insurance cost
+        :param delivery_date: Optional parameter - date of delivery. If not specified - today
         :param base_cost: base cost of cargo
         :param cargo_type: type of cargo. (Ex. Glass, Other)
         :return insurance_cost: Decimal. insurance cost in same currency. We use Decimal because of precision.
         :raises RateDoNotEstablished: if we have no rate for today
         """
         logger.info(f"Calculating insurance cost for cargo with {base_cost} and {cargo_type}")
-        today = date.today()
-        # Get current rate from database with cargo_type. Select only one
-        rate_value = await Rate.filter(date=today, cargo_type=cargo_type).first()
+        delivery_date = delivery_date or date.today()
+        rate_value = await Rate.filter(date=delivery_date, cargo_type=cargo_type).first()
         if rate_value:
             rate = rate_value.rate
+            logger.info(f"Rate for {delivery_date} and {cargo_type} is {rate}")
             return rate * Decimal(base_cost)
         else:
+            logger.warning(f"Rate for {delivery_date} and {cargo_type} is not established")
             raise RateDoNotEstablished
-        # If we have no rate for today, we get last rate
-
 
