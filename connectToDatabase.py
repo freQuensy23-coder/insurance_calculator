cd8219139d737ddd4a173ea7ebdb5880a6b9f185
freQuensy23-coder <mametevalex@gmail.com>
2023-07-19 23:24:43 +0300

    [UPD] Delete depricated database utlis

--- connectToDatabase.py
+++
@@ -1,7 +0,0 @@
-# database/connectToDatabase.py
-from tortoise import Tortoise
-async def connectToDatabase():
-    await Tortoise.init(
-        db_url='sqlite://db.sqlite3',
-        modules={'models': ['database.models']}
-    )
